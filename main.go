c797256a68fc4d934faca521fadfb13ec686391c
cgxarrie <cgxarrie@gmail.com>
2023-05-19 00:18:08 +0200

    clean main.go

--- main.go
+++ main.go
@@ -6,12 +6,6 @@ import (
 
 func main() {
 
-	// config.Instance().Bearer = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9." +
-	// 	"eyJleHAiOjE3MTU4Mzg5NDYsImlzcyI6Imh0dHBzOlwvXC9iYWNrZW5kLnNp" +
-	// 	"Z25pbmFwcC5jb21cLyIsImF1ZCI6Im1vYmlsZS1kZXZpY2UiLCJzdWIiOjkx" +
-	// 	"NTQ5MSwiaWF0IjoxNjg0MzAyOTQ2LCJuYmYiOjE2ODQzMDI5NDZ9.OV8jldk" +
-	// 	"3v3-Ib0N_lg5A14Q-FxMeD5L6MDtQvKnsxOs"
-
 	signin.Execute()
 
 }
