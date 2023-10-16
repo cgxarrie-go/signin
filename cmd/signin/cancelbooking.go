379da24b68097a7d0b139d78fbdc6db1fa7ba324
Carlos Gutierrez Xarrie <carlos.gutierrez@derivco.co.uk>
2023-10-16 14:29:08 +0200

    modify cancel booking to be based on date

--- cmd/signin/cancelbooking.go
+++ cmd/signin/cancelbooking.go
@@ -14,11 +14,11 @@ import (
 var cancelBooking = &cobra.Command{
 	Use:     "cancel",
 	Aliases: []string{"c"},
-	Short:   "cancel a desk reservation",
+	Short:   "cancel desk reservations for a date",
 	Args:    cobra.ExactArgs(1),
-	Example: fmt.Sprintf("signin cancel <ReservationID>\n" +
-		"signin cancel 57387ghf8\n" +
-		"signin c 57387ghf8"),
+	Example: fmt.Sprintf("signin cancel <date>\n" +
+		"signin cancel 20231008\n" +
+		"signin c 202308"),
 	RunE: func(cmd *cobra.Command, args []string) error {
 
 		ctx := context.Background()
@@ -27,7 +27,7 @@ var cancelBooking = &cobra.Command{
 		svc := service.New(client)
 
 		req := service.CancelBookingRequest{
-			BookingID: args[0],
+			Date: args[0],
 		}
 
 		err := svc.CancelBooking(ctx, req)
--- pkg/service/cancelbooking.go
+++ pkg/service/cancelbooking.go
@@ -3,31 +3,42 @@ package service
 import (
 	"context"
 	"fmt"
-	"strconv"
+	"time"
 
 	"github.com/cgxarrie-go/signin/pkg/signin"
+	"github.com/cgxarrie-go/signin/pkg/util"
 )
 
 // CancelBookingRequest .
 type CancelBookingRequest struct {
-	BookingID string
+	Date string
 }
 
 // CancelBooking cancel reservation
 func (s service) CancelBooking(ctx context.Context, req CancelBookingRequest) error {
 
-	id, err := strconv.Atoi(req.BookingID)
+	endDate, err := util.DateFromString(req.Date)
 	if err != nil {
-		return fmt.Errorf("invalid booking ID : %s", req.BookingID)
+		return fmt.Errorf("invalid date: %w", err)
 	}
 
-	clientRequest := signin.CancelBookingRequest{
-		BookingID: id,
+	now := time.Now()
+	startDate := time.Date(now.Year(), now.Month(), now.Day(), 22, 0, 0, 0,
+		now.Location())
+	items, err := s.getListBookingItems(ctx, startDate, endDate)
+	if err != nil {
+		return fmt.Errorf("getting date bookings: %w", err)
 	}
 
-	_, err = s.signinClient.CancelBooking(ctx, clientRequest)
-	if err != nil {
-		return fmt.Errorf("calling signin client: %w", err)
+	for _, item := range items {
+		clientRequest := signin.CancelBookingRequest{
+			BookingID: item.ID,
+		}
+
+		_, err = s.signinClient.CancelBooking(ctx, clientRequest)
+		if err != nil {
+			return fmt.Errorf("calling signin client: %w", err)
+		}
 	}
 
 	return nil
